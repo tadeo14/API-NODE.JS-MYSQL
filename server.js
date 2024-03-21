fbcd6da04bca2ca78709bdc6cfa05656fcb23706
tadeo <tadeoacosta14@gmail.com>
2024-03-21 01:00:07 -0300

    Add dependencies for connecting to a MySQL database and using Nodemon for development. Update package-lock.json and package.json files. Update server.js to use the new dependencies.

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
-const express = require ('express');
+const express = require('express')
+const mysql = require('mysql')
+const myconn = require('express-myconnection')
   
 const app = express()
 app.set('port', process.env.PORT || 9001)
+const dbOptions = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'abc123',
+    database: 'library'
+}
 
+
+
+//middlewares ----------------------
+app.use(myconn(mysql, dbOptions, 'single'))
+
+//routes------------------------------
 app.get('/', (req, res)=> {
     res.send ('welcome to my API')
 }) //ruta principal de nuestra aplicacion
 
+//server running-----------------------------
 app.listen(app.get('port'), () => {
     console.log('Server is running on port', app.get('port')) //mensaje de que esta corriendo el servidor
 }) //puerta que usara 
