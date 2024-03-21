639c9501d8e5608884af26979954e4c028676928
tadeo <tadeoacosta14@gmail.com>
2024-03-21 00:22:26 -0300

    iniciamos el servidor correctamente

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const express = required('express');
+const express = require ('express');
   
 const app = express()
+app.set('port', process.env.PORT || 9000)
 
-app.listen(9000) //puerta que usara 
+app.listen(app.get('port'), () => {
+    console.log('Server is running on port', app.get('port')) //mensaje de que esta corriendo el servidor
+}) //puerta que usara 
