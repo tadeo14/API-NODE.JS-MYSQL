0024d9b4625f48c48263ab449552668863af8937
tadeo <tadeoacosta14@gmail.com>
2024-03-21 00:25:09 -0300

    ruta principal

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require ('express');
 const app = express()
 app.set('port', process.env.PORT || 9000)
 
+app.get('/', (req, res)=> {
+    res.send ('welcome to my API')
+}) //ruta principal de nuestra aplicacion
+
 app.listen(app.get('port'), () => {
     console.log('Server is running on port', app.get('port')) //mensaje de que esta corriendo el servidor
 }) //puerta que usara 
